bdc61b8bf4af8b772a0da685b6408decbf735888
Richard Jones <bedhed50@gmail.com>
2014-10-07 13:40:03 -0600

    Documentation in runner

--- src/chatbot/controller/ChatBotRunner.java
+++ src/chatbot/controller/ChatBotRunner.java
@@ -1,7 +1,16 @@
 package chatbot.controller;
 
+/**
+ * The runner for the ChatBot.
+ * @author rjon7928
+ * @version 10/1/14
+ */
 public class ChatBotRunner
 {
+	/**
+	 * Starting point for all Java programs.
+	 * @param args Unused!
+	 */
 	public static void main(String[] args)
 	{
 		ChatBotAppController appController = new ChatBotAppController();
