fc63ab8ce71ab3abbf929ea7b0a869e7b1ad8543
Richard Jones <bedhed50@gmail.com>
2014-10-21 13:45:27 -0600

    More about layouts

--- src/chatbot/view/ChatBotPanel.java
+++ src/chatbot/view/ChatBotPanel.java
@@ -1,14 +1,13 @@
 package chatbot.view;
 
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.JPanel;
+import javax.swing.*;
 
 import chatbot.controller.ChatBotAppController;
 
-import javax.swing.JButton;
-import javax.swing.JTextField;
-import javax.swing.SpringLayout;
 
 public class ChatBotPanel extends JPanel
 {
@@ -17,41 +16,72 @@ public class ChatBotPanel extends JPanel
 	private JButton firstButton;
 	private JTextField firstTextField;
 	private SpringLayout baseLayout;
+	private JTextArea chatArea;
+	private JScrollPane chatPane;
 	
 	public ChatBotPanel(ChatBotAppController baseController) 
 	{
 		this.baseController = baseController;
 		
-		firstButton = new JButton("Click the button... It clicks the chicks.");
+		firstButton = new JButton("Click the button... It clicks ducks.");
 		firstTextField = new JTextField(25);
 		baseLayout = new SpringLayout();
 		
+		chatArea = new JTextArea(5,20);
+		chatPane = new JScrollPane(chatArea);
+		
+		setupPane();
 		setupPanel();
 		setupLayout();
 		setupListeners();
 		
 	}
 	
+	private void setupPane()
+	{
+		chatArea.setLineWrap(true);
+		chatArea.setWrapStyleWord(true);
+	}
+	
+	/**
+	 * The settings of the panel and how it is setup. (color, size, etc.)
+	 */
 	private void setupPanel()
 	{
 		this.setBackground(Color.BLUE);
 		this.setLayout(baseLayout);
 		this.add(firstButton);
 		this.add(firstTextField);
+		this.add(chatPane);
 	}
 	
+	
+	/**
+	 * Where you store all the code that deals with the layout of the panel.
+	 */
 	private void setupLayout()
 	{
+		/**
+		 * ALWAYS PUT THESE IN THE "setupLayout" METHOD.
+		 */
 		baseLayout.putConstraint(SpringLayout.SOUTH, firstButton, -10, SpringLayout.SOUTH, this);
 		baseLayout.putConstraint(SpringLayout.EAST, firstButton, -33, SpringLayout.EAST, this);
-		baseLayout.putConstraint(SpringLayout.NORTH, firstTextField, 10, SpringLayout.NORTH, this);
-		baseLayout.putConstraint(SpringLayout.WEST, firstTextField, 10, SpringLayout.WEST, this);
-		
+		baseLayout.putConstraint(SpringLayout.SOUTH, firstTextField, -6, SpringLayout.NORTH, firstButton);
+		baseLayout.putConstraint(SpringLayout.EAST, firstTextField, -34, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, chatPane, 50, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, chatPane, 20, SpringLayout.WEST, this);
 	}
 	
 	private void setupListeners()
 	{
-		
+		firstButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				firstTextField.setText(firstTextField.getText()+ " (͡° ͜ʖ ͡°)");
+			}
+		});
+
 	}
 	
 }
